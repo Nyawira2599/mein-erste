0bdb1935e5deb72fa54fb493c25e62f7fc56042a
Nyawira2599 <59165403+Nyawira2599@users.noreply.github.com>
2021-03-12 17:02:16 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<device-width>, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<link rel = "stylesheet" href = "index.css" />
+
+<body >
+    <h1> &nbsp GLO-UP</h1>
+    <nav>
+        <ul>
+            <li><a href = "index.html">HOME</a></li>
+            <li><a href = "proserv.html">PRODUCTS & SERVICES</a></li>
+            <li><a href = "staff.html">STAFF PROFILE</a></li>
+        </ul>
+    </nav><br><br><br><hr>
+    <P>Glo-up is a platform aimed to aid women in finding a sense of style that suits them.<br>
+         From finding the right products to use to the type of clothing that is suitable for their body-type e.t.c<br> 
+        This is to enable them to rediscover themselves as many get caught up taking care of their families or their careers and businesses among many other things.</P>
+
+        <p> We have open positions and searching for suitable candidates to fill them. If interested please check out the link below <a href = "signup.php">Fill out details here</a>,
+             fill in your details and our team will get back to you with further information.
+              We are looking forward to get a new additional member to our team.</p>
+<br>
+<div class = "popeye">
+              <p><a href = "signup.php">Fill out your details here</a></p>
+            </div>
+            <br><br><br>
+            <div class = "copier">
+            <p>&copy; 2020 Glo-up.com<p>
+
+    </div>
+</body>
+</html>
